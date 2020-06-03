7cc3d3631562f7508d8cc9200d38e928f5b73009
HoangDevNull <pqhoang.17it2@sict.udn.vn>
2020-06-03 10:23:48 +0700

    add select answer

--- quiz/helper.js
+++ quiz/helper.js
@@ -1,60 +1,59 @@
 const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 const emptyAnswer = (question, resolve) => {
-    rl.question(question + " not allowed to be empty", (answer) => {
-        resolve(answer);
-    })
-}
+  rl.question(question + " not allowed to be empty", (answer) => {
+    resolve(answer);
+  });
+};
 
 const usernameQuestion = async () => {
-    return new Promise(resolve => {
-        rl.question("enter username", (answer) => {
-            if (answer === "") emptyAnswer("username", resolve);
-            else resolve(answer);
-        })
-    })
-}
+  return new Promise((resolve) => {
+    rl.question("enter username: ", (answer) => {
+      if (answer === "") emptyAnswer("username", resolve);
+      else resolve(answer);
+    });
+  });
+};
 const passwordQuestion = async () => {
-    return new Promise(resolve => {
-        rl.question("enter password", (answer) => {
-            if (answer === "") emptyAnswer("password", resolve);
-            else resolve(answer);
-        })
-    })
-}
+  return new Promise((resolve) => {
+    rl.question("enter password: ", (answer) => {
+      if (answer === "") emptyAnswer("password", resolve);
+      else resolve(answer);
+    });
+  });
+};
 
 const closeBrowser = (browser) => {
-    rl.question("Type exit to Close your browser", (answer) => {
-        if (answer.includes("exit")) {
-            browser.close();
-            process.exit(1);
-        }
-    })
-}
+  rl.question("Type exit to Close your browser: ", (answer) => {
+    if (answer.includes("exit")) {
+      browser.close();
+      process.exit(1);
+    }
+  });
+};
 
 const googleLogin = async (username, password, page) => {
-    // wait for button next
-    await page.waitForSelector('#identifierNext');
-    await page.type('input[type="email"]', username);
-    await page.click('#identifierNext');
+  // wait for button next
+  await page.waitForSelector("#identifierNext");
+  await page.type('input[type="email"]', username);
+  await page.click("#identifierNext");
+  // wait for input show up
+  await page.waitForSelector('input[type="password"]', { visible: true });
+  await page.type('input[type="password"]', password);
 
-    // wait for input show up
-    await page.waitForSelector('input[type="password"]', { visible: true });
-    await page.type('input[type="password"]', password);
+  await page.waitForSelector("#passwordNext", { visible: true });
+  await page.click("#passwordNext");
 
-
-    await page.waitForSelector('#passwordNext', { visible: true });
-    await page.click('#passwordNext');
-}
-
+  await page.waitForNavigation();
+};
 
 module.exports = {
-    googleLogin,
-    usernameQuestion,
-    passwordQuestion,
-    closeBrowser
-}
+  googleLogin,
+  usernameQuestion,
+  passwordQuestion,
+  closeBrowser,
+};
--- quiz/index.js
+++ quiz/index.js
@@ -1,28 +1,50 @@
-const puppeteer = require('puppeteer');
-const { passwordQuestion, usernameQuestion, closeBrowser, googleLogin } = require("./helper");
+const puppeteer = require("puppeteer");
+const {
+  passwordQuestion,
+  usernameQuestion,
+  closeBrowser,
+  googleLogin,
+} = require("./helper");
 
+const { username, password } = require("../config");
 (async () => {
-    const username = await usernameQuestion();
-    const password = await passwordQuestion();
+  //   const username = await usernameQuestion();
+  //   const password = await passwordQuestion();
 
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1366, height: 768 })
-    await page.goto('https://docs.google.com/forms/d/e/1FAIpQLSek-GKkIm87UhaZnpCbMOxmIIDWvlmr-rfjHKR9NAEttOGPtQ/viewform');
-    // wait for page navigate finsished
-    const navigationPromise = page.waitForNavigation();
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1366, height: 768 });
+  await page.goto(
+    "https://docs.google.com/forms/d/e/1FAIpQLSek-GKkIm87UhaZnpCbMOxmIIDWvlmr-rfjHKR9NAEttOGPtQ/viewform"
+  );
+  // wait for page navigate finsished
+  const navigationPromise = page.waitForNavigation();
 
-    // login to google form
-    await googleLogin(username, password, page);
+  // login to google form
+  await googleLogin(username, password, page);
 
-    // wait for page load full content
-    await navigationPromise;
+  // wait for page load full content
+  await navigationPromise;
 
-    const $answers = page.$$(".freebirdFormviewerViewNumberedItemContainer");
+  await page.screenshot({ path: "test.png" });
 
-    (await $answers).forEach(ele => {
-        console.log("log" + ele)
-    })
+  // browser coding
+  await page.evaluate(() => {
+    const questionWrapper = document.querySelectorAll(
+      ".freebirdFormviewerViewItemList .freebirdFormviewerViewNumberedItemContainer"
+    );
 
-    await closeBrowser(browser);
+    // just go throught childnode and get the list of answer
+    var s = Array.from(questionWrapper).map((ele) => {
+      const answerItem =
+        ele.childNodes[0].childNodes[1].childNodes[0].childNodes[0]
+          .childNodes[0].childNodes;
+      return answerItem;
+    });
+
+    // question 1 anwser 1
+    let answer1 = s[0][1].querySelector("label");
+    answer1.click();
+  });
+  await closeBrowser(browser);
 })();
